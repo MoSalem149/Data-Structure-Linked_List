ce7c62c5006909cdb35189e8add9b4d2d3a4cc90
Mohamed Salem <108944781+MoSalem149@users.noreply.github.com>
2023-09-14 02:02:36 +0300

    Update and rename doubleLinkedList.py to Double-Linked-List.py

--- Double-Linked-List.py
+++ Double-Linked-List.py
@@ -1,5 +1,6 @@
+# Define a Node class for individual elements of the linked list
 class Node:
-    def _init_(self, data=None, next=None, prev=None):
+    def __init__(self, data=None, next=None, prev=None):
         self.data = data
         self.next = next
         self.previous = prev
@@ -29,14 +30,17 @@ class Node:
         return self.previous is not None
 
 
+# Define a LinkedList class to manage the linked list
 class LinkedList:
-    def _init_(self):
-        self.head = None
-        self.tail = None
+    def __init__(self):
+        self.head = None  # Initialize the head (start) of the linked list
+        self.tail = None  # Initialize the tail (end) of the linked list
 
     def add_at_beginning(self, item):
+        # Add a new node at the beginning of the linked list
         newNode = Node(item)
         if self.head is None:
+            # If the list is empty, set both head and tail to the new node
             self.head = self.tail = newNode
         else:
             newNode.set_previous(None)
@@ -45,8 +49,10 @@ class LinkedList:
             self.head = newNode
 
     def add_at_end(self, item):
+        # Add a new node at the end of the linked list
         newNode = Node(item)
         if self.head is None:
+            # If the list is empty, set both head and tail to the new node
             self.head = self.tail = newNode
         else:
             newNode.set_next(None)
@@ -55,6 +61,7 @@ class LinkedList:
             self.tail = newNode
 
     def size(self):
+        # Get the size (number of nodes) of the linked list
         current = self.head
         count = 0
         while current is not None:
@@ -63,6 +70,7 @@ class LinkedList:
         return count
 
     def add_at_position(self, item, pos):
+        # Add a new node at a specified position in the linked list
         if pos > self.size() or pos < 0:
             return None
         else:
@@ -78,6 +86,7 @@ class LinkedList:
             current.set_next(newNode)
 
     def remove(self, item):
+        # Remove a node with a specific data value from the linked list
         current = self.head
         found = False
         while not found and current is not None:
@@ -93,6 +102,14 @@ class LinkedList:
                 if current.get_next() is not None:
                     current.get_next().set_previous(current.get_previous())
 
+    def print_list(self):
+        # Print the elements of the linked list
+        current = self.head
+        while current is not None:
+            print(current.get_data(), end=" -> ")
+            current = current.get_next()
+        print("None")  # To indicate the end of the list
+
 
 # Example usage:
 my_list = LinkedList()
@@ -100,4 +117,4 @@ my_list.add_at_beginning(93)
 my_list.add_at_end(42)
 my_list.add_at_end(17)
 my_list.add_at_position(99, 1)
-my_list.print_list()  # Implement the print_list method to display the list
+my_list.print_list()  # This will print the linked list
